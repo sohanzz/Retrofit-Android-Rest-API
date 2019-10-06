358d0efc373479abdafad93818bfc3ad45c105ef
Asif ahmed Sohan <asifsohanz@yahoo.com>
2019-10-06 15:11:33 +0600

    Simple get request

---
+++ app/src/main/java/com/asifahmedsohan/retrofitandroid/JsonPlaceHolderApi.java
@@ -0,0 +1,15 @@
+package com.asifahmedsohan.retrofitandroid;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ * Created by sohan on 06,October,2019
+ */
+public interface JsonPlaceHolderApi {
+
+    @GET("posts")
+    Call<List<Post>> getPost();
+}
